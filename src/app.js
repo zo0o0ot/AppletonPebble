88f7f616a4b48e64439311caef51a2bb08697af5
Ross Larson <zo0o0ot@gmail.com>
2015-07-22 20:38:26 -0500

    Adds user configuration by property Id
    Accessible through web content created using github pages.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 var UI = require('ui');
 var ajax = require('ajax');
 var Vector2 = require('vector2');
+var Settings = require('settings');
 
 // Show splash screen while waiting for data
 var splashWindow = new UI.Window();
+
+// Set a configurable with just the close callback
+Settings.config(
+  { url: 'http://zo0o0ot.github.io/PebblePages/' },
+  function(e) {
+    console.log('closed configurable');
+
+    // Show the parsed response
+    console.log(JSON.stringify(e.options));
+
+    Settings.option('propertyId', e.options.propertyId);
+    // Show the raw response if parsing failed
+    if (e.failed) {
+      console.log(e.response);
+    }
+  }
+);
 
 // Text element to inform user
 var text = new UI.Text({
@@ -21,11 +39,21 @@ var text = new UI.Text({
 splashWindow.add(text);
 splashWindow.show();
 
+var propertyId = Settings.option('propertyId');
+
+if (propertyId === null || propertyId === undefined)
+  {
+    propertyId = 315427100;
+  }
+else
+  {
+    console.log('Property Id is: ' + propertyId);
+  }
 // Make request to Appleton API
 ajax(
   {
     //Proof of Concept - hard coded URL
-    url:'http://appletonapi.appspot.com/property/315427100',
+    url:'http://appletonapi.appspot.com/property/' + propertyId,
     type:'json'
   },
   function(data) {

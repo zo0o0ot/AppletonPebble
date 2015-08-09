647fa1356f75600864ad27aad7e38576fb663753
Ross Larson <zo0o0ot@gmail.com>
2015-08-09 09:41:22 -0500

    Adds local storage of recycle day
    The recycle day is now being stored, and at the beginning of the app, the code now checks for an existing day that is in the future.  If the saved recycle day is still in the future, it just shows that day instead of making another API call.  This parsing and comparison was done with Moment.JS, so we now have the ability to refer to the time as something other than a string, such as changing the format of the date, or using relative time (like "in two days").

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ var UI = require('ui');
 var ajax = require('ajax');
 var Vector2 = require('vector2');
 var Settings = require('settings');
+//moment.js is already there (vendor directory, used by Clock).  I don't need to import it.  CloudPebble doesn't like it, but I can use it simply by typing moment();
+//var Moment = require('moment');
 
 // Show splash screen while waiting for data
 var splashWindow = new UI.Window();
@@ -43,32 +45,58 @@ var propertyId = Settings.option('propertyId');
 
 if (propertyId === null || propertyId === undefined)
   {
+    console.log('Property Id is: ' + propertyId);
     propertyId = 315427100;
   }
 else
   {
     console.log('Property Id is: ' + propertyId);
   }
-// Make request to Appleton API
-ajax(
+
+// Check for existing recycle data that is still relevant.
+var savedRecycleDay = Settings.option('savedRecycleDay');
+console.log('Saved Recycle Day is: ' + savedRecycleDay);
+if (savedRecycleDay !== undefined)
   {
-    //Proof of Concept - hard coded URL
-    url:'http://appletonapi.appspot.com/property/' + propertyId,
-    type:'json'
-  },
-  function(data) {
-    // The location of the recycling day is data[1].recycleday; 
-    var recycleDay = data[1].recycleday;
-    // Create a Card with title and subtitle
+    var tomorrow = moment().add(1, 'd');
+    var savedRecycleMoment = moment(savedRecycleDay, "dddd, MM-DD-YYYY");
+    console.log('Saved Recycle Moment is: ' + moment(savedRecycleMoment).format("dddd, MMMM Do YYYY"));
+  }
+if (savedRecycleDay !== null && savedRecycleDay !== undefined && savedRecycleMoment.isAfter(tomorrow))
+  {
     var card = new UI.Card({
       title:'Next Recycle Day:',
-      subtitle:recycleDay
+      subtitle:savedRecycleDay
     });
-
-    // Display the Card
     card.show();
-  },
-  function(error) {
-    console.log('Download failed: ' + error);
   }
-);
+else
+  {
+    // Make request to Appleton API
+    ajax(
+      {
+        //URL for Appleton API plus the user's property ID.
+        url:'http://appletonapi.appspot.com/property/' + propertyId,
+        type:'json'
+      },
+      function(data) {
+        // The location of the recycling day is data[1].recycleday; 
+        var recycleDay = data[1].recycleday;
+        // Create a Card with title and subtitle
+        var card = new UI.Card({
+          title:'Next Recycle Day:',
+          subtitle:recycleDay
+        });
+    
+        //Save the recycle day to the watch settings storage (Settings.option)
+        Settings.option('savedRecycleDay', recycleDay);
+        // Display the Card
+        card.show();
+      },
+      function(error) {
+        console.log('Download failed: ' + error);
+      }
+    );    
+  }
+
+

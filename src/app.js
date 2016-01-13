ddaf631dcd19eee058db906b9aad8e5f5ee6480d
Ross Larson <zo0o0ot@gmail.com>
2016-01-12 20:18:06 -0600

    updates configuration page reference.

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ var Settings = require('settings');
 
 // Set a configurable with just the close callback
 Settings.config(
-  { url: 'http://mrosack.github.io/AppletonPebble/' },
+  { url: 'http://dhmncivichacks.github.io/AppletonPebble/' },
   function(e) {
     console.log('closed configurable');
 
